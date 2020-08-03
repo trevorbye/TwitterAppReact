7816f970d42d9821a5df445f1bee0b6935a97d3b
trevorbye <trevorcbye2@gmail.com>
2020-08-02 17:48:59 -0700

    building image rendering component

--- ClientApp/src/components/tweet-components/BasicTweetBlock.js
+++ ClientApp/src/components/tweet-components/BasicTweetBlock.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import { TweetImageBlock } from './TweetImageBlock';
 import axios from 'axios';
 
 export class BasicTweetBlock extends Component {
@@ -21,6 +22,18 @@ export class BasicTweetBlock extends Component {
 
                 <p className="mb-1 wrapped">{this.props.tweet.StatusBody}</p>
 
+                {
+                    this.props.tweet.ImageBase64Strings != null &&
+                    <ul className="list-group list-group-horizontal">
+                        {
+                            this.props.tweet.ImageBase64Strings.map((base64, index) => <TweetImageBlock
+                                base64={base64}
+                                key={index}
+                                editPaneExpanded={this.state.editPaneExpanded} />)
+                        }
+                    </ul>
+                }
+                
                 <div className="d-flex w-100 justify-content-between mt-2 mb-3">
                     <span>
                         <i className="far fa-clock fa-sm"></i>
---
+++ ClientApp/src/components/tweet-components/TweetImageBlock.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+
+export class TweetImageBlock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <li className="list-group-item w-25 mr-2 thumbnail-item">
+                {
+                    this.props.editPaneExpanded &&
+                    <i className="fas fa-trash-alt trash-hover float-right mb-2"></i>
+                }
+                <img src={this.props.base64} alt="thumbnail" className="img-thumbnail img-fluid w-100" />
+            </li>
+        );
+    }
+}
