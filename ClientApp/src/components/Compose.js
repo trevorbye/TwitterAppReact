170a8cd99e1e502d0753dac826426fd61f2deec7
trevorbye <trevorcbye2@gmail.com>
2020-08-07 12:41:47 -0700

    built more of compose block

--- ClientApp/src/components/Compose.js
+++ ClientApp/src/components/Compose.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { getAuthHeadersSilent } from './auth-utils/auth-config';
+import { validateTweetBody } from './utils/twitter-text-util.js';
 import axios from 'axios';
 
 export class Compose extends Component {
@@ -8,7 +9,14 @@ export class Compose extends Component {
 
         this.state = {
             globalHandles: [],
-            selectedHandle: ""
+            selectedHandle: "",
+            bodyState: {
+                text: "",
+                isValid: false,
+                bodyLenText: validateTweetBody("").textReturn
+            },
+            dateInput: "",
+            timeInput: ""
         }
     }
 
@@ -29,6 +37,30 @@ export class Compose extends Component {
         });
     }
 
+    bodyChangeHandler(event) {
+        let result = validateTweetBody(event.target.value);
+        this.setState({
+            bodyState: {
+                text: event.target.value,
+                isValid: result.isValid,
+                bodyLenText: result.textReturn
+            }
+        });
+    }
+
+    dateChange(event) {
+        this.setState({
+            dateInput: event.target.value
+        });
+    }
+
+    timeChange(event) {
+        this.setState({
+            timeInput: event.target.value
+        });
+    }
+
+
     render() {
         return (
             <div className="col-md-6">
@@ -48,6 +80,47 @@ export class Compose extends Component {
                         }
                     </select>
                 </div>
+
+                <div className="input-group mb-3">
+                    <label className="sr-only" for="body-text">Tweet text</label>
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">
+                            <i className="fab fa-twitter fa-lg twitter"></i>
+                        </span>
+                    </div>
+                    <textarea className="form-control" 
+                        rows="5" 
+                        id="body-text"
+                        value={this.state.bodyState.text}
+                        onChange={(e) => this.bodyChangeHandler(e)}
+                    >
+                    </textarea>
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="badge">{this.state.bodyState.bodyLenText}</span>
+                </div>
+
+                <div className="input-group mb-3">
+                    <label>Leave either date <strong>or</strong> time blank to post the tweet as soon as it's approved.</label>
+                    <label className="sr-only" for="tweet-date">Schedule tweet date</label>
+                    <label className="sr-only" for="tweet-time">Schedule tweet time</label>
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">
+                            Scheduled date & time
+                        </span>
+                    </div>
+                    <input id="tweet-date" type="date" max="3000-12-31" min="1000-01-01" className="form-control" 
+                        value={this.state.dateInput} 
+                        onChange={(e) => this.dateChange(e)} 
+                    />
+                    <input id="tweet-time" type="time" className="form-control" 
+                        value={this.state.timeInput} 
+                        onChange={(e) => this.timeChange(e)} 
+                    />
+                </div>
+                
+
             </div>
         );
     }
