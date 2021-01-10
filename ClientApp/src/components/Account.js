709f67f876c4c6f620b4c33a2baad9d131206592
diberry <diberry@microsoft.com>
2021-01-10 06:10:11 -0800

    Fix account test - add coverage

--- ClientApp/src/components/Account.js
+++ ClientApp/src/components/Account.js
@@ -130,6 +130,7 @@ export class Account extends Component {
                     <div className="list-group">
                         {
                             this.state.handles.map((handle, idx) => <AccountPane 
+                                key={idx}
                                 handle={handle} 
                                 idx={idx}
                                 disableAutoTweets={(handle, idx) => this.disableAutoTweets(handle, idx)}
