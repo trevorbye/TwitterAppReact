cd3268e4fb92082e351db7c76fcccfe408d7e873
trevorbye <trevorcbye2@gmail.com>
2021-04-20 11:15:55 -0700

    login feedback for guest accounts

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
-import { login } from './auth-utils/auth-config'
+import { login, getAuthHeadersSilent } from './auth-utils/auth-config'
+import axios from 'axios';
 
 export class Login extends Component {
     componentDidMount() {
@@ -8,7 +9,17 @@ export class Login extends Component {
     
     async loginClickHandler() {
         let user = await login(this.props.msalConfig);
-        console.log(user);
+
+        const baseUrl = "https://mstwitterbot.azurewebsites.net/";
+        let authHeaders = await getAuthHeadersSilent(this.props.msalConfig);
+        try {
+            await axios.get(baseUrl + "api/get-distinct-handles", authHeaders);
+        } catch (error) {
+            if (error.response.status === 401) {
+                this.props.updateAuthState(null);
+            }
+        }
+        
         if (user != null) {
             this.props.updateAuthState(user.name);
         }
@@ -23,6 +34,18 @@ export class Login extends Component {
                             <i className="fab fa-microsoft fa-2x align-middle"></i>
                             <span className="align-middle ml-2">Microsoft employee sign in</span>
                         </button>
+
+                        {
+                            this.props.attemptedGuestLogin &&
+                            <div className="alert alert-danger mt-2" role="alert">
+                                <h6 className="alert-heading"><i className="fas fa-info-circle"></i> Note</h6>
+                                <br />
+                                <p className="mb-0">
+                                    ​​​​​​ Guest accounts are not allowed in this application. Please login with your @microsoft.com account.
+                                </p>
+                            </div>
+                        }
+
                     </div>
                 </div>
             </div>
